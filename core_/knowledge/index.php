a96f84eb3443720bf951482ace2a2d9d99fa5881
root <root@103-14-49-160.cprapid.com>
2026-03-28 03:18:30 +0700

    Restructure project from VPS version

--- core_/knowledge/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Bizcity Twin AI — Core\Knowledge
- * (c) 2024-2026 BizCity by Johnny Chu — Made in Vietnam 🇻🇳
- */
-// Silence is golden.
--- core_/knowledge/lib/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Bizcity Twin AI — Core\Knowledge\Lib
- * (c) 2024-2026 BizCity by Johnny Chu — Made in Vietnam 🇻🇳
- */
-// Silence is golden.
